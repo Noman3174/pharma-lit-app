a8a146ac267792005237d7eb448b76304a11919d
Noman3174 <noman3174@gmail.com>
2026-03-03 20:38:12 +0500

    Deploy

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
